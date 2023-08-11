c526e4205212c96e2772b946e17c20dbe6c56c02
Allan Luiz Benedito <allan.benedito@gmail.com>
2023-08-11 17:10:06 -0300

    Setup do Projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
  
   return (
-    <>
-      <div>teste</div>
-        
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>Hello World</h1>
+   </ThemeProvider>
   )
 }
 
