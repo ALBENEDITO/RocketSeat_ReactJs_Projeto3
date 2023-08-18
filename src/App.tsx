ff2f5504a9f53ad9e1db81794ba3c214fd7ca0e2
Allan Luiz Benedito <allan.benedito@gmail.com>
2023-08-18 12:58:28 -0300

    Criando contexto de transações

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
@@ -9,8 +10,10 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <Transactions />
-   </ThemeProvider>
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
+    </ThemeProvider>
   )
 }
 
