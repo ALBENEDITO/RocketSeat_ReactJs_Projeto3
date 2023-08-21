875746b8897e74b12138bca64e372de115b2a7ec
Allan Luiz Benedito <allan.benedito@gmail.com>
2023-08-21 19:29:02 -0300

    Corrigindo erros de linting

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,38 +1,41 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { useContext } from 'react';
-import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { dateFormatter, priceFormatter } from "../../utils/formatter";
-import { SearchForm } from "./components/SearchForm";
-import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
-
-
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
-  
   const { transactions } = useContext(TransactionsContext)
 
   return (
     <div>
       <Header />
       <Summary />
-      
+
       <TransactionsContainer>
-      <SearchForm />
+        <SearchForm />
         <TransactionsTable>
           <tbody>
-          {transactions.map(transaction => {
+            {transactions.map((transaction) => {
               return (
                 <tr key={transaction.id}>
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                    {transaction.type === 'outcome' && '- '}
+                      {transaction.type === 'outcome' && '- '}
                       {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
               )
             })}
@@ -40,5 +43,5 @@ export function Transactions() {
         </TransactionsTable>
       </TransactionsContainer>
     </div>
-  );
+  )
 }
